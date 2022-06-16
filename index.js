3f7efa6e895a936c9a17211ec458d39225f6f76e
Wycliffe Ndiba <w.ndiba@gmail.com>
2022-06-16 12:57:50 +0300

    Done.

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then(response => response.json())
+    .then(data => renderBooks(data))
 }
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
+  books.forEach((book) => {
+    const bookTitle = document.createElement("h2");
+    bookTitle.innerHTML = book.name;
+    document.querySelector("main").appendChild(bookTitle);
   });
 }
-
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
