41be8dd152bc4f2a780ce834d5bcac4304e42a3b
Wycliffe Ndiba <w.ndiba@gmail.com>
2022-06-17 13:55:38 +0300

    Done.

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ function fetchBooks() {
     .then(data => renderBooks(data))
 }
 
+// Solution 1:
 function renderBooks(books) {
   books.forEach((book) => {
     const bookTitle = document.createElement("h2");
@@ -12,3 +13,19 @@ function renderBooks(books) {
     document.querySelector("main").appendChild(bookTitle);
   });
 }
+
+// Solution 2:
+// function renderBooks(books) {
+//   const main= document.querySelector("main");
+//   books.forEach((book) => {
+//     const h2 = document.createElement("h2");
+//     h2.innerHTML = book.name;
+//     main.appendChild(h2);
+//   });
+// }
+
+// // DOMContentLoaded
+// document.addEventListener("DOMContentLoaded", () => {
+//   fetchBooks();
+// }
+// );
